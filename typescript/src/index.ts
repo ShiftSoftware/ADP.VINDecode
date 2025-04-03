48b4d88691b4a2da1d9265f658268d0adb0097a0
kodo-yousif <60068120+kodo-yousif@users.noreply.github.com>
2025-04-03 15:59:28 +0300

    Revert "TypeScript implementation"

--- typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as VIN } from "./VIN"
-export { default as Validator } from "./Validator"
-export {
-  findVINs,
-  normalizeInput,
-  default as VINExtractor,
-  type VINExtractionOptions,
-} from "./VINExtractor"
